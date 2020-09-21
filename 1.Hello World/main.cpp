6c7063629cbb959fdb16df7246a9dd9574317b08
aaonrlai1027 <aaronlai1027@gmail.com>
2020-09-21 17:36:17 -0500

    hw1 done

--- 1.Hello World/main.cpp
+++ 1.Hello World/main.cpp
@@ -8,10 +8,13 @@
 **/
 
 // You need to include some header(s) here!
-
+#include <iostream>
 
 // You need to define your main() function here!
-
+int main() {
+    std::cout << "Hello, world!\nGreetings from Illinois!" << std::endl;
+    return 0;
+}
 
 // Notes:
 
